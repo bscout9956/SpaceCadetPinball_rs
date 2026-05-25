fa8da821a79ba858349cba4c45061686b899d2bb
bscout9956 <bs.blackscout.bs@gmail.com>
2026-05-25 04:16:13 -0300

    Begin implementing t_collision_component with linking to t_edge_segment

---
+++ src/t_collision_component.rs
@@ -0,0 +1,69 @@
+use std::{
+    cell::RefCell,
+    rc::{Rc, Weak},
+};
+
+use crate::t_edge_segment::TEdgeSegment;
+
+// TODO: Temporary
+struct RectF;
+// TODO: Temporary
+struct Vector2;
+// TODO: Temporary
+struct TBall;
+
+#[allow(non_snake_case)]
+pub struct TCollisionComponent {
+    edge_list: Vec<Rc<RefCell<TEdgeSegment>>>,
+    elasticity: f32,
+    smoothness: f32,
+    boost: f32,
+    threshold: f32,
+    soft_hit_sound_id: i32,
+    hard_hit_sound_id: i32,
+    AABB: RectF,
+}
+
+trait TCollisionComponentBehavior {
+    fn collision(
+        &mut self,
+        ball: &mut TBall,
+        next_position: &Vector2,
+        direction: &Vector2,
+        distance: f32,
+        edge: &TEdgeSegment,
+    );
+    fn field_effect(&mut self, ball: &TBall, vec_destination: &mut Vector2) -> i32;
+    fn default_collision(
+        &mut self,
+        ball: &TBall,
+        next_position: &mut Vector2,
+        direction: &mut Vector2,
+    ) -> bool;
+}
+
+impl TCollisionComponentBehavior for TCollisionComponent {
+    fn collision(
+        &mut self,
+        ball: &mut TBall,
+        next_position: &Vector2,
+        direction: &Vector2,
+        distance: f32,
+        edge: &TEdgeSegment,
+    ) {
+        todo!()
+    }
+
+    fn field_effect(&mut self, ball: &TBall, vec_destination: &mut Vector2) -> i32 {
+        todo!()
+    }
+
+    fn default_collision(
+        &mut self,
+        ball: &TBall,
+        next_position: &mut Vector2,
+        direction: &mut Vector2,
+    ) -> bool {
+        todo!()
+    }
+}
--- src/t_edge_segment.rs
+++ src/t_edge_segment.rs
@@ -1,6 +1,8 @@
-use std::cell::Cell;
+use std::cell::{Cell, RefCell};
 use std::ffi::c_void;
-use std::rc::Rc;
+use std::rc::{Rc, Weak};
+
+use crate::t_collision_component::TCollisionComponent;
 
 pub enum WallType {
     Circle = 0,
@@ -13,9 +15,6 @@ pub enum WallValue {
 }
 
 // TODO: Temporary
-pub struct TCollisionComponent {}
-
-// TODO: Temporary
 pub struct TBall {}
 
 // TODO: Temporary
@@ -24,8 +23,8 @@ pub struct RectF {}
 // TODO: Temporary
 pub struct RayType {}
 
-pub struct TEdgeSegment<'a> {
-    pub collision_component: &'a TCollisionComponent,
+pub struct TEdgeSegment {
+    pub collision_component: Weak<RefCell<TCollisionComponent>>,
     pub active_flag: Rc<Cell<bool>>,
     pub processed_flag: bool,
     pub wall_value: WallValue,
@@ -33,14 +32,14 @@ pub struct TEdgeSegment<'a> {
 }
 
 pub trait TEdgeSegmentBehavior {
-    fn edge_collision(&self, ball: TBall, distance: f32);
+    fn edge_collision(&self, ball: &mut TBall, distance: f32);
     fn port_draw(&self);
     fn place_in_grid(&self, aabb: RectF);
     fn find_collision_distance(&self, ray: &RayType) -> f32;
 }
 
-impl<'a> TEdgeSegmentBehavior for TEdgeSegment<'a> {
-    fn edge_collision(&self, ball: TBall, distance: f32) {
+impl TEdgeSegmentBehavior for TEdgeSegment {
+    fn edge_collision(&self, ball: &mut TBall, distance: f32) {
         todo!()
     }
 
@@ -56,14 +55,14 @@ impl<'a> TEdgeSegmentBehavior for TEdgeSegment<'a> {
     }
 }
 
-impl<'a> TEdgeSegment<'a> {
+impl TEdgeSegment {
     pub fn new(
-        coll_comp: &'a TCollisionComponent,
+        coll_comp: &Rc<RefCell<TCollisionComponent>>,
         active_flag: Rc<Cell<bool>>,
         collision_group: u32,
     ) -> Self {
         Self {
-            collision_component: coll_comp,
+            collision_component: Rc::downgrade(coll_comp),
             active_flag,
             processed_flag: false,
             wall_value: WallValue::Empty,
@@ -73,12 +72,12 @@ impl<'a> TEdgeSegment<'a> {
 
     pub fn install_wall(
         float_arr: &[f32],
-        coll_comp: &'a TCollisionComponent,
+        coll_comp: &Rc<RefCell<TCollisionComponent>>,
         active_flag: Rc<Cell<bool>>,
         collision_group: u32,
         offset: u32,
         wall_value: usize,
-    ) -> Box<dyn TEdgeSegmentBehavior + 'a> {
+    ) -> Box<dyn TEdgeSegmentBehavior> {
         todo!()
     }
 }
