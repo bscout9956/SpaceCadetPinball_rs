144fdfe855eb64d1bccc5d9f389cdbc6b98cd2a9
bscout9956 <bs.blackscout.bs@gmail.com>
2026-05-25 19:29:30 -0300

    Make all struct fields public in maths.rs

--- src/maths.rs
+++ src/maths.rs
@@ -1,8 +1,8 @@
 use std::ops::Add;
 
 pub struct Vector2 {
-    x: f32,
-    y: f32,
+    pub x: f32,
+    pub y: f32,
 }
 
 impl PartialEq for Vector2 {
@@ -16,9 +16,9 @@ impl PartialEq for Vector2 {
 }
 
 pub struct Vector3 {
-    x: f32,
-    y: f32,
-    z: f32,
+    pub x: f32,
+    pub y: f32,
+    pub z: f32,
 }
 
 impl Vector3 {
@@ -33,60 +33,60 @@ impl Vector3 {
 }
 
 pub struct Vector2i {
-    x: i32,
-    y: i32,
+    pub x: i32,
+    pub y: i32,
 }
 
 pub struct RectangleType {
-    x_position: i32,
-    y_position: i32,
-    width: i32,
-    height: i32,
+    pub x_position: i32,
+    pub y_position: i32,
+    pub width: i32,
+    pub height: i32,
 }
 
 pub struct CircleType {
-    center: Vector2,
-    radius_sq: f32,
+    pub center: Vector2,
+    pub radius_sq: f32,
 }
 
 pub struct RayType {
-    origin: Vector2,
-    direction: Vector2,
-    max_distance: f32,
-    min_distance: f32,
-    collision_mask: i32,
+    pub origin: Vector2,
+    pub direction: Vector2,
+    pub max_distance: f32,
+    pub min_distance: f32,
+    pub collision_mask: i32,
 }
 
 pub struct LineType {
-    perpendicular: Vector2,
-    direction: Vector2,
-    origin: Vector2,
-    end: Vector2,
-    min_coord: f32,
-    max_coord: f32,
-    ray_intersect: Vector2,
+    pub perpendicular: Vector2,
+    pub direction: Vector2,
+    pub origin: Vector2,
+    pub end: Vector2,
+    pub min_coord: f32,
+    pub max_coord: f32,
+    pub ray_intersect: Vector2,
 }
 
 pub struct WallPointType {
-    pt_0: Vector2,
-    pt_1: Vector2,
+    pub pt_0: Vector2,
+    pub pt_1: Vector2,
 }
 
 pub struct RampPlaneType {
-    ball_collision_offset: Vector3,
-    v1: Vector2,
-    v2: Vector2,
-    v3: Vector2,
-    gravity_angle_1: f32,
-    gravity_angle_2: f32,
-    field_force: Vector2,
+    pub ball_collision_offset: Vector3,
+    pub v1: Vector2,
+    pub v2: Vector2,
+    pub v3: Vector2,
+    pub gravity_angle_1: f32,
+    pub gravity_angle_2: f32,
+    pub field_force: Vector2,
 }
 
 pub struct RectF {
-    x_max: f32,
-    y_max: f32,
-    x_min: f32,
-    y_min: f32,
+    pub x_max: f32,
+    pub y_max: f32,
+    pub x_min: f32,
+    pub y_min: f32,
 }
 
 pub enum FlipperIntersect {
