1cbe1405407eff03f8bb9f5e517a852bd5164313
bscout9956 <bs.blackscout.bs@gmail.com>
2026-05-26 00:32:37 -0300

    Implement t_collision_component (2/?

--- src/t_collision_component.rs
+++ src/t_collision_component.rs
@@ -2,10 +2,13 @@ use std::{
     cell::RefCell,
     rc::{Rc, Weak},
 };
-
+use std::cell::{Cell, Ref};
+use crate::maths;
 use crate::maths::*;
 use crate::t_ball::TBall;
-use crate::t_edge_segment::TEdgeSegment;
+use crate::t_edge_segment::{TEdgeSegment, TEdgeSegmentBehavior};
+use crate::t_pinball_component::{MessageCode, TPinballComponent, TPinballComponentBehavior};
+use crate::t_pinball_table::TPinballTable;
 
 #[allow(non_snake_case)]
 pub struct TCollisionComponent {
@@ -17,9 +20,10 @@ pub struct TCollisionComponent {
     soft_hit_sound_id: i32,
     hard_hit_sound_id: i32,
     AABB: RectF,
+    t_pinball_component: TPinballComponent,
 }
 
-trait TCollisionComponentBehavior {
+pub trait TCollisionComponentBehavior {
     fn collision(
         &mut self,
         ball: &mut TBall,
@@ -31,14 +35,67 @@ trait TCollisionComponentBehavior {
     fn field_effect(&mut self, ball: &TBall, vec_destination: &mut Vector2) -> i32;
     fn default_collision(
         &mut self,
-        ball: &TBall,
-        next_position: &mut Vector2,
+        ball: &mut TBall,
+        next_position: &Vector2,
         direction: &mut Vector2,
     ) -> bool;
 }
 
 impl TCollisionComponent {
-    // TODO: Finish me
+    pub fn new(table: TPinballTable, group_index: i32, create_wall: bool) -> Self {
+        // TODO: Should we pass the table or create a new one?
+        let pinball_table = Rc::new(RefCell::new(TPinballTable::new()));
+        let pinball_component = TPinballComponent::new(
+            Some(pinball_table), group_index, true);
+
+        let visual: VisualStruct;
+
+        let mut Instance = Self {
+            edge_list: vec![],
+            elasticity: 0.0,
+            smoothness: 0.0,
+            boost: 0.0,
+            threshold: 0.0,
+            soft_hit_sound_id: 0,
+            hard_hit_sound_id: 0,
+            AABB: RectF {
+                x_max: -10000.0,
+                y_max: -10000.0,
+                x_min: 10000.0,
+                y_min: 10000.0
+            },
+            t_pinball_component: pinball_component,
+        };
+
+        Instance.t_pinball_component.active_flag = Rc::new(Cell::new(true));
+        if !Instance.t_pinball_component.group_name.is_some() {
+            Instance.t_pinball_component.unused_base_flag = Rc::new(Cell::new(true));
+        }
+        if group_index <= 0{
+            // TODO: Implement loader
+            loader::default::vsi(&visual);
+        }
+        else {
+            // TODO: Implement loader
+            loader::query_visual(group_index, 0, &visual);
+            if (create_wall) {
+                let offset: f32 = table.collision_comp_offset;
+                // TODO: Implement loader
+                let float_arr = loader::query_float_attribute(group_index, 0, 600);
+                TEdgeSegment::install_wall(&float_arr, &Rc::new(RefCell::new(Instance)), Instance.t_pinball_component.active_flag, visual.collision_group, offset, 0);
+            }
+        }
+
+        Instance.threshold = visual.kicker.threshold;
+        Instance.elasticity = visual.elasticity;
+        Instance.smoothness = visual.smoothness;
+        Instance.boost = visual.kicker.boost;
+        Instance.hard_hit_sound_id = visual.kicker.hard_hit_sound_id;
+        Instance.soft_hit_sound_id = visual.soft_hit_sound_id;
+        Instance.t_pinball_component.group_index = group_index;
+
+        Instance
+    }
 }
 
 impl TCollisionComponentBehavior for TCollisionComponent {
@@ -50,19 +107,92 @@ impl TCollisionComponentBehavior for TCollisionComponent {
         distance: f32,
         edge: &TEdgeSegment,
     ) {
-        todo!()
+        if let Some(pinball_table) = self.t_pinball_component.pinball_table {
+            if let Some(upgraded_table) = pinball_table.upgrade() {
+                let table = upgraded_table.borrow();
+
+                if table.tilt_lock_flag {
+                    maths::basic_collision(ball, next_position, direction, self.elasticity, self.smoothness, 1000000000.0, 0.0);
+                    return;
+                }
+            }
+        }
+
+        let proj_speed = maths::basic_collision(
+            ball,
+            next_position,
+            direction,
+            self.elasticity,
+            self.smoothness,
+            self.threshold,
+            self.boost
+        );
+        if proj_speed > self.threshold {
+            loader::play_sound(self.hard_hit_sound_id, ball, "TCollisionComponent1");
+        } else if proj_speed > 0.2 {
+            loader::play_sound(self.soft_hit_sound_id, ball, "TCollisionComponent2");
+        }
     }
 
     fn field_effect(&mut self, ball: &TBall, vec_destination: &mut Vector2) -> i32 {
-        todo!()
+        return 0; // wow
     }
 
     fn default_collision(
         &mut self,
-        ball: &TBall,
-        next_position: &mut Vector2,
+        ball: &mut TBall,
+        next_position: &Vector2,
         direction: &mut Vector2,
     ) -> bool {
+        if let Some (pinball_table) = self.t_pinball_component.pinball_table {
+            if let Some (upgraded_table) = pinball_table.upgrade() {
+                let table = upgraded_table.borrow();
+                if table.tilt_lock_flag {
+                    maths::basic_collision(ball, next_position, direction, self.elasticity, self.smoothness, 1000000000.0, 0.0);
+                    return false;
+                }
+            }
+        }
+        let mut collision = false;
+        let proj_speed = maths::basic_collision(ball, next_position, direction, self.elasticity, self.smoothness, self.threshold, self.boost);
+        if proj_speed > self.threshold {
+            // TODO: implement me
+            loader::play_sound(self.hard_hit_sound_id, ball, "TCollisionComponent1");
+            collision = true;
+        }
+        else if proj_speed > 0.2 {
+            loader::play_sound(self.soft_hit_sound_id, ball, "TCollisionComponent2");
+        }
+        collision
+    }
+}
+
+impl TPinballComponentBehavior for TCollisionComponent {
+    // TODO: Some remain unimplemented I suppose?? Maybe I should look into trait defaults
+    fn sprite_set(index: i32) {
+        todo!()
+    }
+
+    fn sprite_set_ball(index: i32, pos: Vector2i, depth: f32) {
+        todo!()
+    }
+
+    fn get_coordinates(&self) -> Vector2 {
+        todo!()
+    }
+
+    fn get_scoring(&self, index: u32) -> i32 {
+        todo!()
+    }
+
+    fn port_draw(&self) {
+        for edge in &self.edge_list {
+            let edge_borrow = edge.borrow();
+            edge_borrow.port_draw();
+        }
+    }
+
+    fn message(&mut self, code: MessageCode, value: f32) -> MessageCode {
         todo!()
     }
 }
