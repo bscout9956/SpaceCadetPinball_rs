76161afd77ef95b5954923413e145de9d4f223f7
bscout9956 <bs.blackscout.bs@gmail.com>
2026-05-28 01:38:34 -0300

    Do very basic SDL Initialization just for testing.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,16 @@
 #![allow(unused)]
 #![allow(arithmetic_overflow)]
 
-use sdl2::sys::SDL_SetMainReady; // TODO: Remove me when done
+use std::process::exit;
+
+use sdl2::{
+    sys::{
+        SDL_CreateWindow, SDL_INIT_AUDIO, SDL_INIT_EVENTS, SDL_INIT_GAMECONTROLLER,
+        SDL_INIT_JOYSTICK, SDL_INIT_TIMER, SDL_INIT_VIDEO, SDL_Init, SDL_SetMainReady, SDL_Window,
+        SDL_WindowFlags::{SDL_WINDOW_HIDDEN, SDL_WINDOW_RESIZABLE},
+    },
+    video::WindowPos,
+}; // TODO: Remove me when done
 mod gdrv;
 mod group_data;
 mod loader;
@@ -21,5 +30,28 @@ mod zdrv;
 fn main() {
     unsafe {
         SDL_SetMainReady();
+        if (SDL_Init(
+            SDL_INIT_TIMER
+                | SDL_INIT_AUDIO
+                | SDL_INIT_VIDEO
+                | SDL_INIT_EVENTS
+                | SDL_INIT_JOYSTICK
+                | SDL_INIT_GAMECONTROLLER,
+        ) < 0)
+        {
+            println!("OOPS!! No init, closing");
+            exit(1);
+        }
+    }
+    unsafe {
+        println!("Creating window");
+        let window = SDL_CreateWindow(
+            c"PinBall Space Cadet (0.0.0)".as_ptr(),
+            0,
+            0,
+            800,
+            556,
+            SDL_WINDOW_HIDDEN as u32 | SDL_WINDOW_RESIZABLE as u32,
+        );
     }
 }
