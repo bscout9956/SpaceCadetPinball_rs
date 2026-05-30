c46652bf2a9749df1ee0ac54738c2c25646a6af1
bscout9956 <bs.blackscout.bs@gmail.com>
2026-05-30 04:16:24 -0300

    Implement some stuff in fullscrn

--- src/fullscrn.rs
+++ src/fullscrn.rs
@@ -1,21 +1,35 @@
 use crate::options::Setting;
+use crate::pb;
+use std::sync::atomic;
+use std::sync::atomic::AtomicI32;
+use std::sync::atomic::Ordering::SeqCst;
 
-pub(crate) fn set_resolution(p0: _) {
+static RESOLUTION: AtomicI32 = AtomicI32::new(0);
+
+pub fn set_resolution(mut value: i32) {
+    if pb::FULL_TILT_MODE.load(SeqCst) && !pb::FULL_TILT_DEMO_MODE.load(SeqCst) {
+        value = 0;
+    }
+    assert!(value >= 0 && value <= 2, "Resolution value out of bounds");
+    RESOLUTION.store(value, SeqCst);
+}
+
+pub fn get_max_resolution() -> i32 {
+    if pb::FULL_TILT_MODE.load(SeqCst) && !pb::FULL_TILT_DEMO_MODE.load(SeqCst) {
+        2
+    } else {
+        0
+    }
+}
+
+pub fn set_screen_mode(p0: bool) {
     todo!()
 }
 
-pub(crate) fn get_max_resolution() -> _ {
-    todo!()
+pub fn get_resolution() -> i32 {
+    RESOLUTION.load(atomic::Ordering::Acquire)
 }
 
-pub(crate) fn set_screen_mode(p0: bool) {
-    todo!()
-}
-
-pub(crate) fn get_resolution() -> _ {
-    todo!()
-}
-
-pub(crate) fn window_size_changed() {
+pub fn window_size_changed() {
     todo!()
 }
