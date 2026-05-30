b6031825db1df362f1c1ec042ea8d9d46ca8ddd1
bscout9956 <bs.blackscout.bs@gmail.com>
2026-05-30 19:05:02 -0300

    Implement WelfordState and ClockTick functionality

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,7 @@
 #![allow(arithmetic_overflow)]
 extern crate core;
 
-use std::process::exit;
-
+use sdl2::sys::{SDL_GetPerformanceCounter, SDL_GetPerformanceFrequency, SDL_GetTicks};
 use sdl2::{
     sys::{
         SDL_CreateWindow, SDL_INIT_AUDIO, SDL_INIT_EVENTS, SDL_INIT_GAMECONTROLLER,
@@ -11,7 +10,11 @@ use sdl2::{
         SDL_WindowFlags::{SDL_WINDOW_HIDDEN, SDL_WINDOW_RESIZABLE},
     },
     video::WindowPos,
-}; // TODO: Remove me when done
+};
+use std::process::exit;
+use std::sync::atomic::AtomicBool;
+use std::time::Duration as StdDuration;
+use std::time::Instant;
 mod fullscrn;
 mod gdrv;
 mod group_data;
@@ -36,6 +39,68 @@ mod utils;
 
 pub fn restart_func() {
     todo!();
+}
+
+pub struct Duration<const DEN: u64>(i64);
+pub struct TimePoint<const DEN: u64>(Duration<DEN>);
+
+pub trait Clock {
+    type Duration;
+    type TimePoint;
+    const IS_STEADY: bool;
+    unsafe fn now() -> Self::TimePoint;
+}
+
+pub struct SdlPerformanceClock;
+impl Clock for SdlPerformanceClock {
+    type Duration = Duration<1_000_000_000>;
+    type TimePoint = TimePoint<1_000_000_000>;
+    const IS_STEADY: bool = true;
+    unsafe fn now() -> Self::TimePoint {
+        let freq = SDL_GetPerformanceFrequency();
+        let ctr = SDL_GetPerformanceCounter();
+        let whole = (ctr / freq) * 1_000_000_000;
+        let part = (ctr % freq) * 1_000_000_000 / freq;
+        TimePoint(Duration((whole + part) as i64))
+    }
+}
+
+pub struct SdlTickClock;
+impl Clock for SdlTickClock {
+    type Duration = Duration<1_000>;
+    type TimePoint = TimePoint<1_000>;
+    const IS_STEADY: bool = true;
+    unsafe fn now() -> Self::TimePoint {
+        let ticks = SDL_GetTicks();
+        TimePoint(Duration(ticks as i64))
+    }
+}
+
+struct WelfordState {
+    pub mean: f64,
+    pub m2: f64,
+    pub count: i64,
+}
+
+impl WelfordState {
+    fn new() -> Self {
+        Self {
+            mean: 0.005,
+            m2: 0.0,
+            count: 1,
+        }
+    }
+
+    pub fn advance(&mut self, new_value: f64) {
+        self.count += 1;
+        let delta = new_value - self.mean;
+        self.mean += delta / self.count as f64;
+        self.m2 += delta * (new_value - self.mean);
+    }
+
+    pub fn get_std_dev(&self) -> f64 {
+        return f64::sqrt(self.m2 / (self.count - 1) as f64);
+    }
 }
 
 fn main() {
