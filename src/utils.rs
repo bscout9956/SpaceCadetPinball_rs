df2c11a12403f981a7452dfebc04fd4dc35004f0
bscout9956 <bs.blackscout.bs@gmail.com>
2026-05-30 03:46:37 -0300

    Add PATH_SEPARATOR

--- src/utils.rs
+++ src/utils.rs
@@ -3,3 +3,8 @@
 pub fn clamp<T: std::cmp::Ord + Copy>(n: &T, lower: &T, upper: &T) -> T {
     std::cmp::max(*lower, std::cmp::min(*n, *upper))
 }
+
+#[cfg(target_os = "windows")]
+pub const PATH_SEPARATOR: &str = "\\";
+#[cfg(not(target_os = "windows"))]
+pub const PATH_SEPARATOR: &str = "/";
