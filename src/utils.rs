89361d3c442aa52ff7ce2c05dae0924b608ff392
bscout9956 <bs.blackscout.bs@gmail.com>
2026-05-31 18:12:10 -0300

    Add constant PLATFORM_DATA_PATHS

--- src/utils.rs
+++ src/utils.rs
@@ -1,5 +1,8 @@
 // Equivalent to pch.h
 
+use std::ffi::c_char;
+use std::ptr::{addr_of, null};
+
 pub fn clamp<T: std::cmp::Ord + Copy>(n: &T, lower: &T, upper: &T) -> T {
     std::cmp::max(*lower, std::cmp::min(*n, *upper))
 }
@@ -8,3 +11,12 @@ pub fn clamp<T: std::cmp::Ord + Copy>(n: &T, lower: &T, upper: &T) -> T {
 pub const PATH_SEPARATOR: &str = "\\";
 #[cfg(not(target_os = "windows"))]
 pub const PATH_SEPARATOR: &str = "/";
+
+#[cfg(target_os = "windows")]
+pub const PLATFORM_DATA_PATHS: [&str; 0] = [];
+
+#[cfg(not(target_os = "windows"))]
+pub const PLATFORM_DATA_PATHS: [&str; 2] = [
+    "/usr/local/share/SpaceCadetPinball/",
+    "/usr/share/SpaceCadetPinball/",
+];
