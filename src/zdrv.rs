81cfdd0a8af99aea2698944bc200f7da38f09268
bscout9956 <bs.blackscout.bs@gmail.com>
2026-06-01 01:37:51 -0300

    Add extra missing impl for zdrv

--- src/zdrv.rs
+++ src/zdrv.rs
@@ -7,5 +7,18 @@ pub struct ZMapHeaderType {
     pub stride: i32,
     pub resolution: u32,
     pub z_ptr_1: u16,
-    pub texture: SDL_Texture,
+    pub texture: Option<SDL_Texture>,
+}
+
+impl ZMapHeaderType {
+    pub fn new() -> Self {
+        Self {
+            width: 0,
+            height: 0,
+            stride: 0,
+            resolution: 0,
+            z_ptr_1: 0,
+            texture: None,
+        }
+    }
 }
