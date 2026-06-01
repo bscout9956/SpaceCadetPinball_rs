d2ba824e2d001350dee723f0f0f867d9d522f819
bscout9956 <bs.blackscout.bs@gmail.com>
2026-06-01 01:38:17 -0300

    Implement LRead equivalents in utils.rs

--- src/utils.rs
+++ src/utils.rs
@@ -1,10 +1,39 @@
 // Equivalent to pch.h
 
 use std::ffi::c_char;
+use std::io::Read;
 use std::ptr::{addr_of, null};
 
 pub fn clamp<T: std::cmp::Ord + Copy>(n: &T, lower: &T, upper: &T) -> T {
     std::cmp::max(*lower, std::cmp::min(*n, *upper))
+}
+
+pub trait LRead: Sized {
+    fn lread(reader: &mut impl Read) -> std::io::Result<Self>;
+}
+
+impl LRead for u8 {
+    fn lread(reader: &mut impl Read) -> std::io::Result<Self> {
+        let mut buf = [0u8; 1];
+        reader.read_exact(&mut buf)?;
+        Ok(buf[0])
+    }
+}
+
+impl LRead for i16 {
+    fn lread(reader: &mut impl Read) -> std::io::Result<Self> {
+        let mut buf = [0u8; 2];
+        reader.read_exact(&mut buf)?;
+        Ok(i16::from_le_bytes(buf))
+    }
+}
+
+impl LRead for u32 {
+    fn lread(reader: &mut impl Read) -> std::io::Result<Self> {
+        let mut buf = [0u8; 4];
+        reader.read_exact(&mut buf)?;
+        Ok(u32::from_le_bytes(buf))
+    }
 }
 
 #[cfg(target_os = "windows")]
