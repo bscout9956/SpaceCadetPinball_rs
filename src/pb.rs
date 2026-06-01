454f53931faf4b657a2eda87efc49ec544d91a9e
bscout9956 <bs.blackscout.bs@gmail.com>
2026-06-01 01:36:59 -0300

    Add pb init

--- src/pb.rs
+++ src/pb.rs
@@ -1,4 +1,6 @@
+use crate::group_data::DatFile;
 use crate::options::OPTIONS;
+use crate::partman;
 use crate::translations::Msg;
 use sdl2::sys::SDL_MessageBoxFlags;
 use std::cell::RefCell;
@@ -23,6 +25,7 @@ pub static TIME_TICKS: AtomicUsize = AtomicUsize::new(0);
 
 pub static DAT_FILE_NAME: LazyLock<Mutex<String>> = LazyLock::new(|| Mutex::new(String::new()));
 pub static BASE_PATH: LazyLock<Mutex<String>> = LazyLock::new(|| Mutex::new(String::new()));
+pub static RECORD_TABLE: LazyLock<Mutex<Option<DatFile>>> = LazyLock::new(|| Mutex::new(None));
 
 pub fn make_path_name(file_name: &str) -> String {
     match BASE_PATH.lock() {
@@ -91,24 +94,60 @@ pub fn select_dat_file(data_search_paths: &[&str]) {
                 }
 
                 let dat_file_path = make_path_name(&file_name);
-                match File::open(&dat_file_path) {
-                    Ok(file) => {
-                        println!("Opened dat_file: {}", &dat_file_path);
-                        if dat_file_name == "CADET.DAT" {
-                            FULL_TILT_MODE.store(true, Relaxed);
-                        }
-                        if dat_file_name == "DEMO.DAT" {
-                            FULL_TILT_MODE.store(true, Relaxed);
-                            FULL_TILT_DEMO_MODE.store(true, Relaxed);
-                        }
-                        println!("Loading game from: {}", &dat_file_path);
-                        return;
+                if let Err(e) = File::open(&dat_file_path) {
+                    println!("Error opening dat_file {}: {}", &dat_file_path, e);
+                    continue;
+                }
+                println!("Opened dat_file: {}", &dat_file_path);
+                match DAT_FILE_NAME.lock() {
+                    Ok(mut dat_file) => {
+                        *dat_file = file_name;
                     }
                     Err(e) => {
-                        println!("Error opening dat_file {}: {}", &dat_file_path, e);
+                        println!("Error locking DAT_FILE_NAME: {}", e);
                     }
                 }
+                if dat_file_name == "CADET.DAT" {
+                    FULL_TILT_MODE.store(true, Relaxed);
+                }
+                if dat_file_name == "DEMO.DAT" {
+                    FULL_TILT_MODE.store(true, Relaxed);
+                    FULL_TILT_DEMO_MODE.store(true, Relaxed);
+                }
+                println!("Loading game from: {}", &dat_file_path);
+                return;
             }
         }
     }
+}
+
+pub fn init() -> bool {
+    let projection_matrix: [f32; 12] = [0.0; 12];
+
+    let mut data_file_path = String::new();
+    match DAT_FILE_NAME.lock() {
+        Ok(mut file_name) => {
+            if file_name.is_empty() {
+                return false;
+            }
+            data_file_path = make_path_name(&(*file_name));
+        }
+        Err(e) => {
+            println!("Error locking DAT_FILE_NAME: {}", e);
+            return false;
+        }
+    }
+
+    match RECORD_TABLE.lock() {
+        Ok(mut table_opt) => {
+            *table_opt = partman::load_records(data_file_path, FULL_TILT_MODE.load(Relaxed));
+        }
+        Err(e) => {
+            println!("Error locking RECORD_TABLE: {}", e);
+        }
+    }
+
+    let use_bmp_font = false;
+
+    false
 }
