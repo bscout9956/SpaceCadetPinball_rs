cac3a7c814f54029cba78d1bee40ba391baeacda
bscout9956 <bs.blackscout.bs@gmail.com>
2026-06-09 03:55:40 -0300

    Remove temporary structs and new fn from from t_pinball_table.rs

--- src/t_pinball_table.rs
+++ src/t_pinball_table.rs
@@ -1,10 +1,12 @@
 use crate::maths::Vector2;
 use crate::score::ScoreStruct;
 use crate::t_ball::TBall;
+use crate::t_demo::TDemo;
 use crate::t_pinball_component::{MessageCode, TPinballComponent, TPinballComponentBehavior};
 use crate::{control, timer};
 use std::cell::{Cell, RefCell};
 use std::rc::{Rc, Weak};
+use crate::t_light_group::TLightGroup;
 
 pub struct ScoreStructSuper {
     pub score_struct: ScoreStruct,
@@ -20,21 +22,14 @@ pub struct ScoreStructSuper {
 struct TFlipper;
 struct TPlunger;
 struct TDrain;
-struct TDemo;
-struct TLightGroup;
 
-impl TLightGroup {
-    pub(crate) fn message(&self, p0: MessageCode, p1: f64) {
-        todo!()
-    }
-}
 // End of temporary structs
 
 pub struct TPinballTable {
     pub flipper_l: TFlipper,
     pub flipper_r: TFlipper,
     pub cur_score_struct: Option<ScoreStruct>,
-    pub score_ballcount: Option<ScoreStruct>,
+    pub score_ball_count: Option<ScoreStruct>,
     pub score_player_number_1: Option<ScoreStruct>,
     pub cheats_used: i32,
     pub sound_index_1: i32,
@@ -51,7 +46,7 @@ pub struct TPinballTable {
     pub current_player: i32,
     pub plunger: TPlunger,
     pub drain: TDrain,
-    pub demo: TDemo,
+    pub demo: Option<TDemo>,
     pub x_offset: i32,
     pub y_offset: i32,
     pub width: i32,
@@ -59,7 +54,7 @@ pub struct TPinballTable {
     pub component_list: Vec<Rc<RefCell<TPinballComponent>>>,
     pub ball_list: Vec<TBall>,
     pub flipper_list: Vec<TFlipper>,
-    pub light_group: TLightGroup,
+    pub light_group: Option<TLightGroup>,
     pub gravity_dir_vect_mult: f32,
     pub gravity_angle_x: f32,
     pub gravity_angle_y: f32,
@@ -94,129 +89,10 @@ unsafe impl Sync for TPinballTable {}
 unsafe impl Send for TPinballTable {}
 
 impl TPinballTable {
-    pub fn new() -> Self {
-        let active_flag = Rc::new(Cell::new(true));
-
-        let instance = Self {
-            active_flag: active_flag.clone(),
-            flipper_l: TFlipper,
-            flipper_r: TFlipper,
-            cur_score_struct: None,
-            score_ballcount: None,
-            score_player_number_1: None,
-            cheats_used: 0,
-            sound_index_1: 0,
-            sound_index_2: 0,
-            sound_index_3: 0,
-            ball_in_drain_flag: 0,
-            cur_score: 0,
-            cur_score_e9: 0,
-            light_show_timer: 0,
-            end_game_timeout_timer: 0,
-            tilt_timeout_timer: 0,
-            player_scores: [
-                ScoreStruct {
-                    score: todo!(),
-                    dirty_flag: todo!(),
-                    background_bmp: todo!(),
-                    offset_x: todo!(),
-                    offset_y: todo!(),
-                    width: todo!(),
-                    height: todo!(),
-                    char_bmp: todo!(),
-                    msg_font: todo!(),
-                },
-                ScoreStruct {
-                    score: todo!(),
-                    dirty_flag: todo!(),
-                    background_bmp: todo!(),
-                    offset_x: todo!(),
-                    offset_y: todo!(),
-                    width: todo!(),
-                    height: todo!(),
-                    char_bmp: todo!(),
-                    msg_font: todo!(),
-                },
-                ScoreStruct {
-                    score: todo!(),
-                    dirty_flag: todo!(),
-                    background_bmp: todo!(),
-                    offset_x: todo!(),
-                    offset_y: todo!(),
-                    width: todo!(),
-                    height: todo!(),
-                    char_bmp: todo!(),
-                    msg_font: todo!(),
-                },
-                ScoreStruct {
-                    score: todo!(),
-                    dirty_flag: todo!(),
-                    background_bmp: todo!(),
-                    offset_x: todo!(),
-                    offset_y: todo!(),
-                    width: todo!(),
-                    height: todo!(),
-                    char_bmp: todo!(),
-                    msg_font: todo!(),
-                },
-            ],
-            player_count: 0,
-            current_player: 0,
-            plunger: TPlunger,
-            drain: TDrain,
-            demo: TDemo,
-            x_offset: 0,
-            y_offset: 0,
-            width: 0,
-            height: 0,
-            component_list: vec![],
-            ball_list: vec![],
-            flipper_list: vec![],
-            light_group: TLightGroup,
-            gravity_dir_vect_mult: 0.0,
-            gravity_angle_x: 0.0,
-            gravity_angle_y: 0.0,
-            collision_comp_offset: 0.0,
-            // TODO What do? Verify me
-            plunger_position: Vector2 { x: 0.0, y: 0.0 },
-            score_multiplier: 0,
-            score_added: 0,
-            reflex_shot_score: 0,
-            bonus_score: 0,
-            bonus_score_flag: false,
-            jackpot_score: 0,
-            jackpot_score_flag: false,
-            unknown_p71: 0,
-            ball_count: 0,
-            max_ball_count: 0,
-            extra_balls: 0,
-            multiball_count: 0,
-            ball_locked_counter: 0,
-            multiball_flag: false,
-            unknown_p78: 0,
-            replay_active_flag: 0,
-            replay_timer: 0,
-            unknown_p81: 0,
-            unknown_p82: 0,
-            tilt_lock_flag: false,
-            score_multipliers: vec![1, 1, 1, 1, 1], // TODO: change me
-        };
-
-        // TODO: Implement AddBall
-        //let ball = AddBall(Vector2 { x: 0.0, y: 0.0 });
-        //ball.disable();
-
-        // TODO: Implement TTableLayer
-        //TTableLayer::new(this);
-
-        //let score1 = score::create("score1", render::background_bitmap);
-        //Instance.cur_score_struct = Some(score1);
-        //Instance.player_scores[0] = score1;
-        // TODO: Continue implementing
-
-        instance
+    pub fn new() {
+        todo!()
     }
-
+    
     pub fn message(&mut self, code: MessageCode, value: f32) -> i32 {
         let rc_text = String::new();
 
@@ -231,7 +107,7 @@ impl TPinballTable {
                 self.replay_timer = 0;
                 if self.light_show_timer > 0 {
                     timer::kill_id(self.light_show_timer);
-                    self.light_group
+                    self.light_group.unwrap()
                         .message(MessageCode::T_LIGHT_GROUP_RESET, 0.0);
                 }
                 self.light_show_timer = 0;
