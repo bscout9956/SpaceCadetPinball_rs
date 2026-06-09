b92c965d4cbe4c72511a75425abb4da272550314
bscout9956 <bs.blackscout.bs@gmail.com>
2026-06-09 18:28:14 -0300

    Stub more of control

--- src/control.rs
+++ src/control.rs
@@ -1,5 +1,31 @@
-use crate::t_pinball_component::MessageCode;
+use crate::t_pinball_component::{MessageCode, TPinballComponent};
 
 pub(crate) fn table_control_handler(p0: MessageCode) {
     todo!()
 }
+
+struct ComponentTagBase {
+    name: &'static str,
+}
+
+impl ComponentTagBase {
+    fn new(name: &'static str) -> Self {
+        Self { name }
+    }
+}
+
+pub trait ComponentTagBaseBehavior {
+    fn get_component(&self) -> Option<TPinballComponent>;
+    fn set_component(comp: Option<TPinballComponent>);
+}
+
+pub struct ComponentControl {
+    pub score_count: u32,
+    pub scores: Vec<i32>,
+}
+
+struct ComponentInfo {
+    tag: ComponentTagBase,
+    control: ComponentControl,
+}
+
