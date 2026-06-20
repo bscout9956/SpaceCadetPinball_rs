8930b7daa1c79655637c556764dec802969118d7
bscout9956 <bs.blackscout.bs@gmail.com>
2026-06-20 16:39:43 -0300

    Stub trait in t_light

--- src/t_light.rs
+++ src/t_light.rs
@@ -1,5 +1,7 @@
+use crate::message_code::MessageCode;
 use crate::t_pinball_component::{IPinballComponent, TPinballComponent};
 use std::any::Any;
+use crate::maths::{Vector2, Vector2i};
 
 pub struct TlightPlayerBackup {
     message_field: i32,
@@ -29,18 +31,44 @@ pub struct TLight {
 }
 
 impl IPinballComponent for TLight {
-    fn group_index(&self) -> i32 {
-        self.base.group_index
-    }
-
     fn as_any(&self) -> &dyn Any {
         self
     }
+
     fn group_name(&self) -> Option<String> {
         if let Some(name) = self.base.group_name.as_ref() {
             let name_str = name.borrow().to_string();
             return Some(name_str);
         }
         None
+    }
+
+    fn group_index(&self) -> i32 {
+        self.base.group_index
+    }
+    fn sprite_set(&mut self, index: i32) {
+        todo!()
+    }
+
+    fn sprite_set_ball(&self, index: i32, pos: Vector2i, depth: f32) {
+        todo!()
+    }
+
+    fn get_coordinates(&self) -> Vector2 {
+        todo!()
+    }
+
+    fn get_scoring(&self, index: u32) -> i32 {
+        todo!()
+    }
+
+    fn port_draw(&self) {
+        todo!()
+    }
+
+    fn message(&mut self, code: MessageCode, value: f32) -> i32 {
+        println!("Beep boop");
+        //TODO finish me
+        0
     }
 }
