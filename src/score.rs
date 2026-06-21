57fd05c68361d1853f5a062621531dabe9599616
bscout9956 <bs.blackscout.bs@gmail.com>
2026-06-21 06:28:27 -0300

    Avoid ginourmous stack allocations by using vec in Score.rs

--- src/score.rs
+++ src/score.rs
@@ -6,16 +6,16 @@ use crate::state::pinball_state::PinballState;
 use crate::state::score_state::ScoreState;
 use std::sync::{Arc, RwLock};
 
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 pub struct ScoreStruct {
     pub score: i32,
     pub dirty_flag: bool,
-    pub background_bmp: GdrvBitmap8,
+    pub background_bmp: Option<GdrvBitmap8>,
     pub offset_x: i32,
     pub offset_y: i32,
     pub width: i32,
     pub height: i32,
-    pub char_bmp: [GdrvBitmap8; 10],
+    pub char_bmp: Vec<GdrvBitmap8>,
     pub msg_font: ScoreMessageFontType,
 }
 
@@ -24,18 +24,18 @@ impl Default for ScoreStruct {
         Self {
             score: 0,
             dirty_flag: false,
-            background_bmp: GdrvBitmap8::default(),
+            background_bmp: Some(GdrvBitmap8::default()),
             offset_x: 0,
             offset_y: 0,
             width: 0,
             height: 0,
-            char_bmp: std::array::from_fn(|_| GdrvBitmap8::default()),
+            char_bmp: (0..10).map(|_| GdrvBitmap8::default()).collect(),
             msg_font: ScoreMessageFontType::new(),
         }
     }
 }
 
-#[derive(Clone, PartialEq)]
+#[derive(Clone, PartialEq, Debug)]
 pub struct ScoreMessageFontType {
     pub gap_width: i32,
     pub height: i32,
@@ -47,7 +47,7 @@ impl ScoreMessageFontType {
         Self {
             gap_width: 0,
             height: 0,
-            chars: Vec::with_capacity(128),
+            chars: Vec::new(),
         }
     }
 }
