b7e1080b0cf1fa170184d68daeda312b7af76348
bscout9956 <bs.blackscout.bs@gmail.com>
2026-06-25 03:42:51 -0300

    Use Rc<RefCell> for field_list

--- src/t_edge_box.rs
+++ src/t_edge_box.rs
@@ -3,7 +3,8 @@ use crate::t_edge_segment::IEdgeSegment;
 use std::cell::RefCell;
 use std::rc::Rc;
 
+#[derive(Default, Clone)]
 pub struct TEdgeBox {
     pub edge_list: Vec<Rc<RefCell<dyn IEdgeSegment>>>,
-    pub field_list: Vec<Option<FieldEffectType>>,
+    pub field_list: Vec<Rc<RefCell<FieldEffectType>>>,
 }
