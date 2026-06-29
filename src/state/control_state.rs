cca9de48e357645dcf9c1cb3d813b84e095224d4
bscout9956 <bs.blackscout.bs@gmail.com>
2026-06-29 17:17:07 -0300

    Add more states to control_state

--- src/state/control_state.rs
+++ src/state/control_state.rs
@@ -6,10 +6,12 @@ use crate::control::ComponentInfo;
 pub struct ControlState {
     pub table_g: Option<Weak<RefCell<TPinballTable>>>,
     pub score_components: Vec<ComponentInfo>,
+    pub easy_mode: bool,
+    pub table_unlimited_balls: bool
 }
 
 impl ControlState {
     pub fn new() -> ControlState {
-        Self { table_g: None, score_components: vec![] }
+        Self { table_g: None, score_components: vec![], easy_mode: false, table_unlimited_balls: false }
     }
 }
