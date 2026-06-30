4a9e7a30a2b05dec72d3da63cf320f0af6a5cf3d
bscout9956 <bs.blackscout.bs@gmail.com>
2026-06-30 01:15:13 -0300

    Stop using 'static str in TTextBoxMessage

--- src/t_textbox_message.rs
+++ src/t_textbox_message.rs
@@ -1,20 +1,20 @@
 #[derive(PartialEq, Clone)]
 pub struct TTextBoxMessage {
     pub next_message: Option<Box<TTextBoxMessage>>,
-    pub text: &'static str,
+    pub text: String,
     pub time: f32,
     pub end_ticks: usize,
     pub low_priority: bool,
 }
 
 impl TTextBoxMessage {
-    pub fn new(text: &'static str, time: f32, low_priority: bool, time_ticks: usize) -> Self {
+    pub fn new(text: &str, time: f32, low_priority: bool, time_ticks: usize) -> Self {
         Self {
             next_message: None,
             time,
             end_ticks: time_ticks + (time * 1000.0f32) as usize,
             low_priority,
-            text,
+            text: String::from(text),
         }
     }
 
