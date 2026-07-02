b079352bc412617ecac33bd6d62ccba6b3075fce
bscout9956 <bs.blackscout.bs@gmail.com>
2026-07-02 01:08:33 -0300

    Begin implementing T_Blocker

---
+++ src/t_blocker.rs
@@ -0,0 +1,189 @@
+use crate::loader::VisualStruct;
+use crate::maths::{RectF, Vector2, Vector2i};
+use crate::message_code::MessageCode;
+use crate::render::RenderSprite;
+use crate::state::pinball_state::PinballState;
+use crate::t_ball::TBall;
+use crate::t_collision_component::{ICollisionComponent, TCollisionComponent};
+use crate::t_edge_segment::{IEdgeSegment, TEdgeSegment};
+use crate::t_pinball_component::IPinballComponent;
+use crate::t_pinball_table::TPinballTable;
+use crate::{control, loader, timer};
+use anyhow::Result;
+use std::any::Any;
+use std::cell::{Ref, RefCell};
+use std::ffi::c_void;
+use std::ops::Deref;
+use std::pin::Pin;
+use std::rc::{Rc, Weak};
+
+pub struct TBlocker {
+    pub base: TCollisionComponent,
+    pub initial_duration: i32,
+    pub extended_duration: i32,
+    pub timer: i32,
+    pub sound_index_4: i32,
+    pub sound_index_3: i32,
+}
+
+impl TBlocker {
+    fn new(
+        table: Option<Weak<RefCell<TPinballTable>>>,
+        group_index: i32,
+        state: &mut PinballState,
+    ) -> Result<Self> {
+        let base = TCollisionComponent::new(table, group_index, true, state)?;
+        base.borrow_mut().threshold = 1000000000.0f32;
+        base.borrow_mut().message_field = MessageCode(0);
+        base.borrow_mut().sprite_set(-1);
+
+        let mut visual = VisualStruct::default();
+        loader::query_visual(group_index, 0, &mut visual, state)?;
+
+        let base_strong = Rc::deref(&base).clone().into_inner();
+
+        Ok(Self {
+            base: base_strong,
+            initial_duration: 55,
+            extended_duration: 5,
+            timer: 0,
+            sound_index_4: visual.sound_index_4,
+            sound_index_3: visual.sound_index_3,
+        })
+    }
+
+    pub unsafe extern "C" fn timer_expired(
+        timer_id: i32,
+        caller: *mut c_void,
+        pinball_state: &mut PinballState,
+    ) {
+        unsafe {
+            let blocker = &mut *(caller as *mut TBlocker);
+            (*blocker).timer = 0;
+            control::handler(MessageCode::CONTROL_TIMER_EXPIRED, blocker);
+        }
+    }
+}
+
+impl IPinballComponent for TBlocker {
+    fn render_sprite(&self) -> Option<&RenderSprite> {
+        todo!()
+    }
+
+    fn as_any(&self) -> &dyn Any {
+        todo!()
+    }
+
+    fn group_name(&self) -> Option<String> {
+        todo!()
+    }
+
+    fn group_index(&self) -> i32 {
+        todo!()
+    }
+
+    fn sprite_set(&mut self, index: i32) {
+        todo!()
+    }
+
+    fn sprite_set_ball(&self, index: i32, pos: Vector2i, depth: f32) {
+        todo!()
+    }
+
+    fn get_coordinates(&self) -> Vector2 {
+        todo!()
+    }
+
+    fn get_scoring(&self, index: u32) -> i32 {
+        todo!()
+    }
+
+    fn port_draw(&self) {
+        todo!()
+    }
+
+    fn message(&mut self, code: MessageCode, value: f32, time_ticks: usize) -> i32 {
+        match code {
+            MessageCode::SET_TILT_LOCK
+            | MessageCode::PLAYER_CHANGED
+            | MessageCode::RESET
+            | MessageCode::T_BLOCKER_DISABLE => {
+                if self.timer > 0 {
+                    // TODO: This is very likely wrong;
+                    timer::kill_id(self.timer);
+                    self.timer = 0;
+                }
+                self.base.message_field = MessageCode(0);
+                self.base.active_flag.set(false);
+                self.base.sprite_set(-1);
+                if code == MessageCode::T_BLOCKER_DISABLE {
+                    // TODO:                    loader::play_sound(self.sound_index_3, self, "TBlocker1");
+                }
+            }
+            MessageCode::T_BLOCKER_ENABLE => {
+                self.base.active_flag.set(true);
+                // TODO: loader::play_sound(self.sound_index_4, self, "TBlocker2");
+                self.base.sprite_set(0);
+                if self.timer > 0 {
+                    // TODO: Likely wrong
+                    timer::kill_id(self.timer);
+                }
+                self.timer = 0;
+                if value >= 0.0f32 {
+                    self.timer = timer::set(
+                        value,
+                        &raw const *self as *mut c_void,
+                        Self::timer_expired,
+                        time_ticks,
+                    );
+                }
+            }
+            MessageCode::T_BLOCKER_RESTART_TIMEOUT => {
+                if self.timer > 0 {
+                    timer::kill_id(self.timer);
+                }
+                self.timer = timer::set(
+                    f32::max(value, 0.0f32),
+                    &raw const *self as *mut c_void,
+                    Self::timer_expired,
+                    time_ticks,
+                );
+            }
+            _ => {
+
+            }
+        }
+
+        0
+    }
+
+    fn set_active_flag(&mut self, active: bool) {
+        todo!()
+    }
+}
+
+impl ICollisionComponent for TBlocker {
+    fn collision(
+        &mut self,
+        ball: &mut TBall,
+        next_position: &Vector2,
+        direction: &mut Vector2,
+        distance: f32,
+        edge: &TEdgeSegment,
+        time_ticks: usize,
+    ) {
+        todo!()
+    }
+
+    fn edge_list(&mut self) -> &mut Vec<Rc<RefCell<dyn IEdgeSegment>>> {
+        todo!()
+    }
+
+    fn set_AABB(&mut self, aabb: RectF) {
+        todo!()
+    }
+
+    fn get_AABB(&self) -> Option<RectF> {
+        todo!()
+    }
+}
