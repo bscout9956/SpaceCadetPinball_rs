5bc13668636adc36e5b65393ced0ab13072d479b
bscout9956 <bs.blackscout.bs@gmail.com>
2026-07-02 01:08:47 -0300

    Stub T_Sound

---
+++ src/t_sound.rs
@@ -0,0 +1,15 @@
+use crate::t_pinball_component::IPinballComponent;
+use std::cell::RefCell;
+use std::rc::Rc;
+
+pub struct TSound;
+
+impl TSound {
+    pub(crate) fn play(
+        &self,
+        sound_source: Option<Rc<RefCell<dyn IPinballComponent>>>,
+        info: &str,
+    ) -> f32 {
+        todo!()
+    }
+}
