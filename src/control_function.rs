65c8921ef4a2a2d713d5709364a6df292288592b
bscout9956 <bs.blackscout.bs@gmail.com>
2026-07-10 17:07:31 -0300

    Update control functions

--- src/control_function.rs
+++ src/control_function.rs
@@ -15,20 +15,104 @@ use std::rc::Rc;
 
 pub(crate) fn bumper_control(
     code: MessageCode,
-    caller: Rc<RefCell<dyn IPinballComponent>>,
-    mut table_opt: Option<Rc<RefCell<TPinballTable>>>,
-    full_tilt_mode: bool,
+    caller: &mut dyn IPinballComponent,
+    ctx: &mut ComponentContext,
 ) -> Result<()> {
-    let caller_borrow = caller.borrow();
-    let t_bumper_opt = caller_borrow.as_tbumper();
+    let t_bumper_opt = caller.as_tbumper();
     if code == MessageCode::CONTROL_COLLISION
-        && let Some(table) = table_opt.as_mut() && let Some(t_bumper) = t_bumper_opt {
-            table.borrow_mut().add_score(
-                caller
-                    .borrow()
-                    .get_scoring(t_bumper.bmp_index as u32),
-                full_tilt_mode,
+        && let Some(table) = ctx.main_table.as_ref()
+        && let Some(t_bumper) = t_bumper_opt
+    {
+        table.borrow_mut().add_score(
+            caller.get_scoring(t_bumper.bmp_index as u32),
+            ctx.full_tilt_mode,
+        )?;
+    }
+    Ok(())
+}
+
+pub(crate) fn right_kicker_gate_control(
+    code: MessageCode,
+    _caller: &mut dyn IPinballComponent,
+    ctx: &mut ComponentContext,
+) -> Result<()> {
+    if code == MessageCode::T_GATE_DISABLE {
+        ctx.control_state
+            .component_state
+            .lite_29
+            .get()
+            .unwrap()
+            .borrow_mut()
+            .message(
+                MessageCode::T_LIGHT_FLASHER_START_TIMED_THEN_STAY_ON,
+                5.0,
+                ctx,
             )?;
-        }
+        ctx.control_state
+            .component_state
+            .lite_195
+            .get()
+            .unwrap()
+            .borrow_mut()
+            .message(MessageCode::T_LIGHT_FLASHER_START_TIMED, 5.0, ctx)?;
+    } else if code == MessageCode::T_GATE_ENABLE {
+        ctx.control_state
+            .component_state
+            .lite_29
+            .get()
+            .unwrap()
+            .borrow_mut()
+            .message(MessageCode::T_LIGHT_RESET_AND_TURN_OFF, 0.0, ctx)?;
+        ctx.control_state
+            .component_state
+            .lite_195
+            .get()
+            .unwrap()
+            .borrow_mut()
+            .message(MessageCode::T_LIGHT_RESET_AND_TURN_OFF, 0.0, ctx)?;
+    }
+    Ok(())
+}
+
+pub(crate) fn left_kicker_gate_control(
+    code: MessageCode,
+    _caller: &mut dyn IPinballComponent,
+    ctx: &mut ComponentContext,
+) -> Result<()> {
+    if code == MessageCode::T_GATE_DISABLE {
+        ctx.control_state
+            .component_state
+            .lite_30
+            .get()
+            .unwrap()
+            .borrow_mut()
+            .message(
+                MessageCode::T_LIGHT_FLASHER_START_TIMED_THEN_STAY_ON,
+                5.0,
+                ctx,
+            )?;
+        ctx.control_state
+            .component_state
+            .lite_196
+            .get()
+            .unwrap()
+            .borrow_mut()
+            .message(MessageCode::T_LIGHT_FLASHER_START_TIMED, 5.0, ctx)?;
+    } else if code == MessageCode::T_GATE_ENABLE {
+        ctx.control_state
+            .component_state
+            .lite_30
+            .get()
+            .unwrap()
+            .borrow_mut()
+            .message(MessageCode::T_LIGHT_RESET_AND_TURN_OFF, 0.0, ctx)?;
+        ctx.control_state
+            .component_state
+            .lite_196
+            .get()
+            .unwrap()
+            .borrow_mut()
+            .message(MessageCode::T_LIGHT_RESET_AND_TURN_OFF, 0.0, ctx)?;
+    }
     Ok(())
 }
