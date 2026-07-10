d836c4491a5ad93248faeb36d90be01f3aea9df7
bscout9956 <bs.blackscout.bs@gmail.com>
2026-07-10 17:06:02 -0300

    Remove duplicate light

--- src/state/component_state.rs
+++ src/state/component_state.rs
@@ -11,7 +11,6 @@ use std::rc::{Rc, Weak};
 pub struct ComponentState {
     pub info_text_box: ComponentRef<TTextBox>,
     pub mission_text_box: ComponentRef<TTextBox>,
-    pub lite_62: ComponentRef<TLight>,
     pub soundwave_7: ComponentRef<TSound>,
     pub soundwave_28: ComponentRef<TSound>,
     pub block_1: ComponentRef<TBlocker>,
